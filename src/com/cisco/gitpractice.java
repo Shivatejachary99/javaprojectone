88d951915270fc5d5605d37afa43c03ace99e637
shivateja <shiva.tejas99@gmail.com>
2023-06-07 11:00:53 +0530

    first commit

---
+++ src/com/cisco/gitpractice.java
@@ -0,0 +1,18 @@
+package com.cisco;
+
+public class gitpractice {
+	
+	public void add() {
+		int a = 21;
+		int b = 22;
+		int c = a+b;
+		System.out.println("a = "+a);
+		System.out.println("b = "+b);
+		System.out.println("a+b = c = "+c);
+	}
+
+	public static void main(String[] args) {
+		gitpractice a = new gitpractice();
+		a.add();
+	}
+}
